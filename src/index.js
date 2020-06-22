50342f042fdcc97c02c947dc4c646d28f6e99116
matheus.goncalves <matheusvieira1900@hotmail.com>
2020-06-21 21:30:04 -0300

    Adicionando e corrigindo alguns exemplos de problemas

--- src/index.js
+++ src/index.js
@@ -45,6 +45,9 @@ function start() {
 
   const output = YASMIJ.solve(dataForSimplexOperation);
 
+  console.log('\nDados informados:');
+  console.log(dataForSimplexOperation);
+  console.log('\nResultado final:');
   console.log(output);
 }
 
@@ -53,46 +56,46 @@ start();
 /** First Problem
  * Maximizar 5x + 2y
  * Sujeito a:
- * x <= 3
- * y <= 4
- * x + 2y <= 9
+x <= 3
+y <= 4
+x + 2y <= 9
  * x, y >= 0
  **/
 
 /** Second Problem
  * Minimizar 6x + 10y
  * Sujeito a:
- * – x + y <= 2
- * x + 2y >= 1
- * x <= 5
- * x <= 6
- * 3x + 5y >= 15
- * 5x + 4y >= 20
+– x + y <= 2
+x + 2y >= 1
+x <= 5
+x <= 6
+3x + 5y >= 15
+5x + 4y >= 20
  * x, y >= 0
  **/
 
 /** Third Problem
  * Maximizar x + 2y
  * sujeito a:
- * 3x + 4y <= 24
- * 5x + 2y <= 20
+3x + 4y <= 24
+5x + 2y <= 20
  * x, y >= 0
  **/
 
 /** Fourth Problem
  * Maximizar 80x + 60y
  * sujeito a:
- * 4x + 6y <= 24
- * 4x + 2y <= 16
- * y <= 3
+4x + 6y <= 24
+4x + 2y <= 16
+y <= 3
  * x, y >= 0
  **/
 
 /** Fifth Problem
  * Minimizar 0.006x + 0.008y
  * sujeito a:
- * 0.1x + 0.2y >= 2
- * 0.4x + 0.6y >= 64
- * 0.5x + 0.2y >= 34
+0.1x + 0.2y >= 2
+0.4x + 0.6y >= 64
+0.5x + 0.2y >= 34
  * x, y >= 0
  **/
